c19d0cc2e68808a14d416c083b59a48588fb22f1
nosirxon650 <nurmetovnosirxon668@gmail.com>
2025-01-29 17:58:05 +0500

    bug fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,71 +1,81 @@
-import React from 'react'
-import AlifIcon from "../assets/icons/AlifIcon"
-import MenuIcon from "../assets/icons/MenuIcon"
-import CloseIcon from "../assets/icons/CloseIcon"
-import SearchIcon from "../assets/icons/SearchIcon"
-import LikeIcon from "../assets/icons/LikeIcon"
-import SavatIcon from "../assets/icons/SavatIcon"
+import React from "react";
+import AlifIcon from "../assets/icons/AlifIcon";
+import MenuIcon from "../assets/icons/MenuIcon";
+import SearchIcon from "../assets/icons/SearchIcon";
+import LikeIcon from "../assets/icons/LikeIcon";
+import SavatIcon from "../assets/icons/SavatIcon";
 
 function Header() {
     return (
-        <header className="header">
-            <div className="container">
-                <div className="header__wrap">
-                    <div className="header__top">
-                        <div className="header__left">
-                            <a href="/" className='header__logo'>
+        <header className='header'>
+            <div className='container'>
+                <div className='header__wrap'>
+                    <div className='header__top'>
+                        <div className='header__left'>
+                            <a href='/' className='header__logo'>
                                 <AlifIcon />
                             </a>
-                            <button className="header__catalog">
+                            <button className='header__catalog'>
                                 <MenuIcon />
                                 Tovarlar katalogi
                             </button>
                         </div>
 
-                        <div className="header__search">
-                            <input className='header__input' type="text" placeholder='Tovarlarni izlash' />
+                        <div className='header__search'>
+                            <input
+                                className='header__input'
+                                type='text'
+                                placeholder='Tovarlarni izlash'
+                            />
                             {/* <CloseIcon /> */}
-                            <button className="header__search-icon">
+                            <button className='header__search-icon'>
                                 <SearchIcon />
                             </button>
                         </div>
-                        <a href='/' className="header__button">
+                        <a href='/' className='header__button'>
                             <SavatIcon />
                             Savat
-                        </a >
-                        <a href='/' className="header__button">
+                        </a>
+                        <a href='/' className='header__button'>
                             <LikeIcon />
                             Saralanganlar
                         </a>
-                        <button className="header__signup">
-                            Kirish
-                        </button>
+                        <button className='header__signup'>Kirish</button>
 
                         <select className='header__language'>
-                            <option value="UZB">UZB</option>
-                            <option value="РУС">РУС</option>
+                            <option value='UZB'>UZB</option>
+                            <option value='РУС'>РУС</option>
                         </select>
                     </div>
-                    <div className="header__bottom">
-                        <div className="header__box">
-                            <p className="header__subtitle">Smartfonlar va gadjetlar</p>
-                            <p className="header__subtitle">Noutbuklar, kompyuterlar</p>
-                            <p className="header__subtitle">TV va proektorlar</p>
-                            <p className="header__subtitle">Audiotexnikalar</p>
-                            <p className="header__subtitle">Transport</p>
-                            <p className="header__subtitle">Uy uchun texnika</p>
-                            <p className="header__subtitle">Oshxona uchun texnika</p>
-                            <p className="header__subtitle">Go'zallik va sog'liq</p>
+                    <div className='header__bottom'>
+                        <div className='header__box'>
+                            <p className='header__subtitle'>
+                                Smartfonlar va gadjetlar
+                            </p>
+                            <p className='header__subtitle'>
+                                Noutbuklar, kompyuterlar
+                            </p>
+                            <p className='header__subtitle'>
+                                TV va proektorlar
+                            </p>
+                            <p className='header__subtitle'>Audiotexnikalar</p>
+                            <p className='header__subtitle'>Transport</p>
+                            <p className='header__subtitle'>Uy uchun texnika</p>
+                            <p className='header__subtitle'>
+                                Oshxona uchun texnika
+                            </p>
+                            <p className='header__subtitle'>
+                                Go'zallik va sog'liq
+                            </p>
                         </div>
-                        <div className="header__box1">
-                            <h1 className="header__title">Yana</h1>
+                        <div className='header__box1'>
+                            <h1 className='header__title'>Yana</h1>
                         </div>
-
                     </div>
                 </div>
             </div>
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
